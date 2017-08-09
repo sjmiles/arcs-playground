35b41dd3142b90f7860eac33486095c3fa944dbb
Scott J Miles <sjmiles@google.com>
2017-08-09 12:15:09 -0700

    most basic product list

--- configure.js
+++ configure.js
@@ -13,20 +13,12 @@ window.urls = {
 
 window.db = {
   entities: [
-    'Person',
     'Product'
   ],
   views: {
-    'peopleFromWebpage': 'Person',
-    'productsFromWebpage': 'Product',
-    'personSlot': 'Person'
+    //'whocares?': 'Product',
   },
   model: {
-    Person: [
-      {
-        name: "Claire"
-      }
-    ],
     Product: [
       {
         name: "Minecraft Book",
@@ -58,7 +50,6 @@ window.db = {
 
 window.recipes = [
 {
-  name: "Create shortlist with [product, ...]",
   particles: [{
     name: "Create",
     constrain: {
@@ -75,121 +66,5 @@ window.recipes = [
       "list": "list"
     }
   }]
-}, 
-
-{
-  name: "See [person]'s wishlist",
-  particles: [{
-    name: "WishlistFor",
-    constrain: {
-      "wishlist": "wishlist",
-      "person": "person"
-    }
-  },{
-    name: "Choose",
-    constrain: {
-      "singleton": "person"
-    }
-  },{
-    name: "ShowProducts",
-    constrain: {
-      "list": "wishlist"
-    }
-  }]
-},
-
-{
-  name: "Create shortlist with [product, ...] and suggest similar products from [person]'s wish list",
-  particles: [{
-    name: "Create",
-    constrain: {
-      "newList": "list"
-    }
-  },{
-    name: "Create",
-    constrain: {
-      "newList": "recommended"
-    }
-  },{
-    name: "WishlistFor",
-    constrain: {
-      "wishlist": "wishlist",
-      "person": "person"
-    }
-  },{
-    name: "Recommend",
-    constrain: {
-      "known": "list",
-      "population": "wishlist",
-      "recommendations": "recommended"
-    }
-  },{
-    name: "SaveList",
-    constrain: {
-      "list": "list"
-    }
-  },{
-    name: "Choose",
-    constrain: {
-      "singleton": "person"
-    }
-  },{
-    name: "ShowProducts",
-    constrain: {
-      "list": "list"
-    }
-  },{
-    name: "Chooser",
-    constrain: {
-      "choices": "recommended",
-      "resultList": "list"
-    }
-  }, {
-    name: "AlsoOn",
-    constrain: {
-      "list": "list",
-      "choices": "wishlist"
-    }
-  }]
-}, 
-
-{
-  name: "Buying for [person]'s [occasion] in [timeframe]? Product [X] arrives too late.",
-  particles: [{
-    name: "GiftList",
-    constrain: {
-      "person": "person"
-    }
-  }, {
-    name: "Arrivinator",
-    constrain: {
-      "list": "list"
-    }
-  }]
-}, 
-
-{
-  name: "Check manufacturer information for products.",
-  particles: [{
-    name: "ManufacturerInfo",
-    constrain: {
-      "list": "list"
-    }
-  }]
-}, 
-
-{
-  name: "Recommendations based on Claire's interest in field hockey.",
-  particles: [{
-    name: "Interests",
-    constrain: {
-      "list": "wishlist"
-    }
-  }]
-}, 
-
-{
-  name: "Profit",
-  particles: []
 }
 ];
